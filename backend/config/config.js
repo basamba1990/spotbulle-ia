64d7b475a90d5c8b7e9ec00aed9803c0c1b46762
basamba1990 <basamba1990@yahoo.fr>
2025-06-16 23:31:36 +0000

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     // Utilisez l'URL de la base de données pour le développement.
     // Vous pouvez la définir directement ici ou via une variable d'environnement locale.
-    url: process.env.DATABASE_URL || 'postgresql://user:password@localhost:5432/your_dev_db',
+    url: process.env.DATABASE_URL || 'postgresql://spotbulle_ia_db_user:jskCEaL3eumFWab6WHJw2cIX9ovtpcFo@dpg-d16tl7ndiees73djcl90-a/spotbulle_ia_db',
     dialect: 'postgres',
     logging: console.log, // Active les logs en développement pour voir ce que fait Sequelize
   },
