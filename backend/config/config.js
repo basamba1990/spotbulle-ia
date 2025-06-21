b0a4d7d89e12492b804bab70b39f4a87d3102847
basamba1990 <basamba1990@yahoo.fr>
2025-06-21 00:48:44 +0000

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,25 +1,25 @@
 // backend/config/config.js
-require(\'dotenv\').config(); // Nécessaire pour charger les variables d\'environnement si vous testez en local
+require("dotenv").config(); // Nécessaire pour charger les variables d'environnement si vous testez en local
 
 module.exports = {
   development: {
-    // Utilisez l\'URL de la base de données pour le développement.
-    // Vous pouvez la définir directement ici ou via une variable d\'environnement locale.
-    url: process.env.DATABASE_URL || \'postgresql://spotbulle_ia_db_deoi_user:sHwvy4I5c530CeBYFHlAyuPgJQI4x2r6@dpg-d191qhbuibrs73bo0ut0-a.frankfurt-postgres.render.com/spotbulle_ia_db_deoi\',
-    dialect: \'postgres\',
+    // Utilisez l'URL de la base de données pour le développement.
+    // Vous pouvez la définir directement ici ou via une variable d'environnement locale.
+    url: process.env.DATABASE_URL || "postgresql://spotbulle_ia_db_deoi_user:sHwvy4I5c530CeBYFHlAyuPgJQI4x2r6@dpg-d191qhbuibrs73bo0ut0-a.frankfurt-postgres.render.com/spotbulle_ia_db_deoi",
+    dialect: "postgres",
     logging: console.log, // Active les logs en développement pour voir ce que fait Sequelize
   },
   test: {
-    // Configuration pour l\'environnement de test.
-    url: process.env.DATABASE_URL_TEST || \'postgresql://user:password@localhost:5432/your_test_db\',
-    dialect: \'postgres\',
+    // Configuration pour l'environnement de test.
+    url: process.env.DATABASE_URL_TEST || "postgresql://user:password@localhost:5432/your_test_db",
+    dialect: "postgres",
     logging: false,
   },
   production: {
-    // Configuration pour l\'environnement de production sur Render.
-    // Sequelize utilisera la variable d\'environnement DATABASE_URL définie sur Render.
+    // Configuration pour l'environnement de production sur Render.
+    // Sequelize utilisera la variable d'environnement DATABASE_URL définie sur Render.
     url: process.env.DATABASE_URL,
-    dialect: \'postgres\',
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true, // Exige une connexion SSL
@@ -29,5 +29,3 @@ module.exports = {
     logging: false, // Désactive les logs en production pour des raisons de performance et de sécurité
   }
 };
-
-
