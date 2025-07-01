9f17b2f769a6421cd2e9b382eaf4f764e3b5066f
basamba1990 <basamba1990@yahoo.fr>
2025-07-01 03:17:28 +0000

    Add files via upload

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: {
+    // Assurez-vous que tailwindcss est le premier plugin
     tailwindcss: {},
     autoprefixer: {},
+    // Si vous utilisez d'autres plugins PostCSS, ajoutez-les ici
   },
-}
+};
+
 
