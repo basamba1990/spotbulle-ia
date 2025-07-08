615e07b277ffd3c35f553c20ea67cf9038420fc6
basamba1990 <basamba1990@yahoo.fr>
2025-07-08 14:46:13 +0000

    Add files via upload

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config/db");
+const { sequelize } = require("../../config/db-production");
 const bcrypt = require("bcryptjs"); // CHANGEMENT ICI
 
 const User = sequelize.define("User", {
